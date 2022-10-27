00649efbdeda4dc2029bf3012fe9775eba83a726
Drifterino <EnryUnt@gmail.com>
2022-10-27 04:39:43 -0400

    Literally all the basic code logic

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+rfid = {
+    "10053": "Devon Barker",
+    "10054": "Henry Hunt",
+    "10055": "Ravioli",
+    "10056": "Billium",
+}
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+scanning = True
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+people_in_building = []
+while scanning:
+    scanned_id = input()
+    if scanned_id in people_in_building:
+        people_in_building.remove(scanned_id)
+        print(f"Good Bye, {rfid[scanned_id]}")
+    else:
+        people_in_building.append(scanned_id)
+        print(f"Welcome, {rfid[scanned_id]}")
+    for people in people_in_building:
+        print(rfid[people])
